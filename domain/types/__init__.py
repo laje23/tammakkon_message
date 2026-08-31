428d6972a0622926a37f717f2e3ec8acb26933d7
laje23 <a763545@gmail.com>
2026-08-31 20:05:44 +0330

    added

--- domain/types/__init__.py
+++ domain/types/__init__.py
@@ -1,2 +1,3 @@
 from .category_error_type import CategoryErrorsType
 from .platform_type import PlatformType
+from .message_type import MessageType
---
+++ domain/types/message_type.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class MessageType(Enum):
+    TEXT="text"
+    PHOTO="photo"
+    VIDEO="video"
+    AUDIO="audio"
+    DOCUMENT="document"
