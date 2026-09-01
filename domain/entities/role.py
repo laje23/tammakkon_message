5f9aa51c4f2f30339a4eb2f62f4ae1754f754071
laje23 <a763545@gmail.com>
2026-09-01 19:57:32 +0330

    roll entity

---
+++ domain/entities/role.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass , field 
+from datetime import datetime 
+
+
+@dataclass
+class Role :
+    id : int|None
+    name : str 
+    descrtiption: str | None
+    created_at : datetime = field(default_factory=datetime.now)
+    updated_at = datetime |None = None
+    
+    
+    def update(self , name:str |None = None ,descrtiption: str |None=None ):
+        if name is not None :
+            self.name = name
+        
+        if descrtiption is not None :
+            self.descrtiption = descrtiption
+        
+        self.updated_at = datetime.now()
+        
