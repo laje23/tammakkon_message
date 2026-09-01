f06af048ab0ea9489d7bf4f1dfbfb7a2212d7c6c
laje23 <a763545@gmail.com>
2026-09-01 10:29:47 +0330

    inits

--- domain/entities/__init__.py
+++ domain/entities/__init__.py
@@ -1,1 +1,6 @@
 from .bot_account import BotAccount
+from .destination import Destination
+from .log import Log
+from .media import Media
+from .message import Message
+from .message_target import MessageTarget
