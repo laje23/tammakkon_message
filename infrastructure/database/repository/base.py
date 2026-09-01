891d9cad56aa9914d1975f2dc063a2c49e842b26
laje23 <a763545@gmail.com>
2026-09-01 21:47:31 +0330

    base repository

---
+++ infrastructure/database/repository/base.py
@@ -0,0 +1,39 @@
+from typing import Generic, TypeVar
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from infrastructure.database.base import Base
+
+
+ModelT = TypeVar("ModelT", bound=Base)
+
+
+class SQLAlchemyRepository(Generic[ModelT]):
+
+    def __init__(self, session: Session):
+        self.session = session
+
+    def create(self, model: ModelT) -> ModelT:
+        self.session.add(model)
+        self.session.flush()
+        self.session.refresh(model)
+
+        return model
+
+    def get_by_id(self, model_id: int) -> ModelT | None:
+        return self.session.get(self.model, model_id)
+
+    def get_all(self) -> list[ModelT]:
+        statement = select(self.model)
+
+        result = self.session.scalars(statement)
+
+        return list(result)
+
+    def delete(self, model: ModelT) -> None:
+        self.session.delete(model)
+
+    @property
+    def model(self) -> type[ModelT]:
+        raise NotImplementedError
